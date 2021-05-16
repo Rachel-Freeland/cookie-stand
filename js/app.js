ad96bc7f82fc81c784347cb410ed3b4e89d7de21
Rachel Freeland <rachel.freeland2@gmail.com>
2021-05-15 21:40:20 -0500

    Changes to JS

--- js/app.js
+++ js/app.js
@@ -1,30 +1,61 @@
 'use strict';
 
-// Create an array for store hours
-const storeHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
-
-// Create an array to hold store locations
-const storeLocations = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
-
-
-
 // Create separate object literals for each shop
+// Store the min / max hourly customers and the average cookies per customer in obj. properties
+
+function randomCustomersPerHour(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 const seattleStore = {
   store: 'Seattle',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookies: 6.3,
-  avgCustHour: [],
-  setAvgCustomer: function() {
-    this.avgCustHour = randomCustomerPerHour(23, 65);
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiesPerCustomer: 6.3,
+  customersPerHour: 0,
+  setCustomersPerHour: function() {
+    console.log(this.customersPerHour);
+    this.customersPerHour = this.randomCumstomersPerHour(this.minCustomers, this.maxCustomers);
+    console.log(this.customersPerHour);
   }
 };
 
-function randomCustomerPerHour(min, max) {
-  return Math.ceil(Math.random() * (max - min) + min);
-}
+const tokyoStore = {
+  store: 'Tokyo',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookies: 1.2,
+};
+
+const dubaiStore = {
+  store: 'Dubai',
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookies: 3.7,
+};
+
+const parisStore = {
+  store: 'Paris',
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookies: 2.3,
+};
+
+const limaStore = {
+  store: 'Lima',
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookies: 4.6,
+};
 
 
 
-console.log(seattleStore.avgCustomerHour);
+
+
+
+
+console.log(seattleStore);
+console.log(tokyoStore);
+console.log(dubaiStore);
+console.log(parisStore);
+console.log(limaStore);
