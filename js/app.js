c9feb05fa0a9b29ad65378a14d34fa4371fc76d6
Rachel Freeland <rachel.freeland2@gmail.com>
2021-05-16 10:29:40 -0500

    Work on the randomFunction until it worked

--- js/app.js
+++ js/app.js
@@ -2,56 +2,71 @@
 
 // Create separate object literals for each shop
 // Store the min / max hourly customers and the average cookies per customer in obj. properties
+// Use a method of the object the object to generate a random number of customers per hour
+// Calculate & store the simulated amounts of cookies purchased each hour at each location using avgCookiesPerCustomer and the randomCustomersPerHour()
 
-function randomCustomersPerHour(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
 
 const seattleStore = {
   store: 'Seattle',
   minCustomers: 23,
   maxCustomers: 65,
   avgCookiesPerCustomer: 6.3,
-  customersPerHour: 0,
-  setCustomersPerHour: function() {
-    console.log(this.customersPerHour);
-    this.customersPerHour = this.randomCumstomersPerHour(this.minCustomers, this.maxCustomers);
-    console.log(this.customersPerHour);
+  storeHours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
+  cookiesSoldPerHour: [],
+  totalCookiesPerDay: 0,
+  setCookiesSoldPerHour: function() {
+    return this.cookiesSoldPerHour.push(Math.ceil(randomCustomersPerHour(this.minCustomers, this.maxCustomers) * this.avgCookiesPerCustomer));
   }
 };
+
+for (let i = 0; i < seattleStore.storeHours.length; i++) {
+  seattleStore.setCookiesSoldPerHour();
+  seattleStore.totalCookiesPerDay += seattleStore.cookiesSoldPerHour[i];
+  console.log(seattleStore.totalCookiesPerDay, seattleStore.cookiesSoldPerHour);
+}
 
 const tokyoStore = {
   store: 'Tokyo',
   minCustomer: 3,
   maxCustomer: 24,
-  avgCookies: 1.2,
+  avgCookiesPerCustomer: 1.2,
+  storeHours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
 };
 
 const dubaiStore = {
   store: 'Dubai',
   minCustomer: 11,
   maxCustomer: 38,
-  avgCookies: 3.7,
+  avgCookiesPerCustomer: 3.7,
+  storeHours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
 };
 
 const parisStore = {
   store: 'Paris',
   minCustomer: 20,
   maxCustomer: 38,
-  avgCookies: 2.3,
+  avgCookiesPerCustomer: 2.3,
+  storeHours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
 };
 
 const limaStore = {
   store: 'Lima',
   minCustomer: 2,
   maxCustomer: 16,
-  avgCookies: 4.6,
+  avgCookiesPerCustomer: 4.6,
+  storeHours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
 };
 
+function randomCustomersPerHour(minCustomers, maxCustomers) {
+  return Math.floor(Math.random() * (maxCustomers - minCustomers) + minCustomers);
+}
 
-
-
-
+// let totalCookiesPerDay = 0;
+// for (let i = 0, i < storeHours.length, i++) {
+//   setCookiesSoldPerHour();
+//   totalCookiesPerDay += cookiesSoldPerHour[i];
+//   console.log(totalCookiesPerDay, cookiesSoldPerHour);
+// }
 
 
 console.log(seattleStore);
